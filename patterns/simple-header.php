af88d49d3e12fd2f76be312327e9ebfe27db3f63
Kelley Muro <kelleymuro@gmail.com>
2023-09-12 15:04:25 -0700

    Added navigation pattern

---
+++ patterns/simple-header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Simple Header
+ * Slug: simple-header
+ * Description: This is a simple header with a logo and cart icon
+ * Categories: Navigation
+ * Keywords: north commerce, navigation, header
+ * Viewport Width: 600
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+
+?>
+
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-title /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4} /-->
+
+<!-- wp:shortcode -->
+[nc-cart-icon]
+<!-- /wp:shortcode --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
